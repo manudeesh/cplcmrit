092fa7b68aa8065c0d544084b7ea34da540cb484
manudeesh <101454196+manudeesh@users.noreply.github.com>
2022-03-12 15:58:59 +0530

    file upload 1

---
+++ manupg3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	
+	printf("enter the first number");
+	scanf("%d",&a);
+	printf("ur first number:=%d",a);
+ 	
+	printf("enter the second");
+	scanf("%d",&b);
+	printf("ur second number:=%d",b);
+	c=a+b;
+	printf("\n %d",c);
+	return 0;
+}
